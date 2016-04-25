1e09f518619f98ce1acddf54d786a536e8d88fe8
Archon Team <rezafardipour2@gmail.com>
2016-04-25 10:00:17 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -274,16 +274,8 @@ function create_config( )
     sudo_users = {147237496},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[avast V6.3 - Open Source
-    http://bit.ly/1SjFZ2e
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/Arashalone/avast-BOT-V6.3/
-
-Developed and avast team owner: @arashnomiri
-
-Our channel:
-@avast_team
+    about_text = [[Soft TG V2
+An advance Administration bot based on Soft TG
 ]],
     help_text_realm = [[
 Realm Commands:
